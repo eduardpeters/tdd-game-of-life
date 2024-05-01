58ad563bab8012d62777bb2378c9385d78a20dfc
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-05-01 19:44:07 +0200

    Read and return RLE file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
+import fs from 'node:fs';
+import path from 'path';
+
 export default function main(filepath: string, generations: number) {
   if (filepath === undefined || generations === undefined) {
     throw new Error('Two arguments should be supplied');
   }
-  return;
+
+  const file = fs.readFileSync(path.join(__dirname, filepath), 'utf-8');
+
+  return file;
 }
--- test/endToEnd.spec.ts
+++ test/endToEnd.spec.ts
@@ -1,3 +1,5 @@
+import fs from 'node:fs';
+import path from 'path';
 import { describe, expect, test } from 'vitest';
 import main from '../src/index';
 
@@ -5,18 +7,24 @@ describe('Running the main application', () => {
   test('An error is thrown if no arguments are provided', () => {
     const filepath = '../patters/block.rle';
 
+    //@ts-expect-error
     expect(() => main()).toThrowError();
   });
 
   test('An error is thrown if no generations are specified', () => {
     const filepath = '../patters/block.rle';
 
+    //@ts-expect-error
     expect(() => main(filepath)).toThrowError();
   });
 
-  test.skip('An error is thrown if no arguments are provided', () => {
-    const filepath = '../patters/block.rle';
+  test('A RLE file is read and returned', () => {
+    const filepath = '../patterns/block.rle';
+    const generations = 1;
 
-    expect(() => main()).toThrowError();
+    const rlePath = path.join(__dirname, filepath);
+    const fileString = fs.readFileSync(rlePath, 'utf-8');
+
+    expect(main(filepath, generations)).toEqual(fileString);
   });
 });
